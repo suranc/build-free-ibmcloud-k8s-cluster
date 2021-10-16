d46af2584185da5cfb0c3f386859b845d2fc620e
Chris Suran <suran.c@gmail.com>
2021-10-16 13:07:35 -0400

    Remove line wrapping for kubeconfig base64 encoding

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,4 +60,4 @@ echo "::set-output name=ingress-ip::$INGRESS_IP"
 echo "::set-output name=ingress-port::$INGRESS_PORT"
 
 # Output kubeconfig file in base64
-echo "::set-output name=kubeconfig::$(cat ~/.kube/config|base64)"
+echo "::set-output name=kubeconfig::$(cat ~/.kube/config|base64 -w 0)"
